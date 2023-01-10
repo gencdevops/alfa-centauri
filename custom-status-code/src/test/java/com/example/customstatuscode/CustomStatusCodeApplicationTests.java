f6d1ce5784dc9173869e9781b56ef9cd8a99b346
Taner GENC <taner.genc@consultant.turkcell.com.tr>
2023-01-10 18:08:45 +0300

    init

---
+++ custom-status-code/src/test/java/com/example/customstatuscode/CustomStatusCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customstatuscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomStatusCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
