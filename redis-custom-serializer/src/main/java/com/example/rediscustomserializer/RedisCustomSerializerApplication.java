6aa10f54a5b553f4a953d00b9395a81cb4857e7d
Taner GENC <taner.genc@consultant.turkcell.com.tr>
2023-01-10 18:20:49 +0300

    [Taner]-redis

---
+++ redis-custom-serializer/src/main/java/com/example/rediscustomserializer/RedisCustomSerializerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediscustomserializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCustomSerializerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCustomSerializerApplication.class, args);
+    }
+
+}
