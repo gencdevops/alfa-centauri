99e933f113d68280ba7297f9785ddd9d5bbeeab9
Oguzcan BICER <oguzcanbicer96@gmail.com>
2023-02-28 16:39:14 +0300

    [Oguzcan BICER]-restaurant-system initial commit

---
+++ restaurant-system/cg-config-server/src/test/java/com/example/cgconfigserver/CgConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ restaurant-system/cg-registry/src/test/java/com/example/cgregistry/CgRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
