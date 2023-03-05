598cf08f273ea4caf28d9cb3762e88e516416671
furkanozmen <ozmenf97@gmail.com>
2023-03-05 11:03:46 +0300

    unit test added

---
+++ restaurant-system/cg-order/src/test/java/com/example/cgorder/BaseIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.example.cgorder;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@AutoConfigureMockMvc
+@ActiveProfiles("application-integration")
+@ExtendWith({SpringExtension.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseIntegrationTest {
+}
