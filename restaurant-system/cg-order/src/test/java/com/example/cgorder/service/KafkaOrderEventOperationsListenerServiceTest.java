e4e506d616eb5c8853f41a7626cd20d5307e1151
Burhan Taşkesen <taskesenburhanoff@gmail.com>
2023-03-06 10:41:09 +0300

    [Burhan TAŞKESEN] KafkaOrderEventOperationsListenerService unit test isimlendirme

--- restaurant-system/cg-order/src/test/java/com/example/cgorder/service/KafkaOrderEventOperationsListenerServiceTest.java
+++ restaurant-system/cg-order/src/test/java/com/example/cgorder/service/KafkaOrderEventOperationsListenerServiceTest.java
@@ -30,7 +30,7 @@ class KafkaOrderEventOperationsListenerServiceTest {
     KafkaOrderEventOperationsListenerService kafkaOrderEventOperationsListenerService;
 
     @Test
-    void handleMessage() throws JsonProcessingException {
+    void handleMessage_Should_SetStatusAndSaveOrder() throws JsonProcessingException {
         UUID uuid = UUID.randomUUID();
 
         Order mockOrder = Mockito.spy(Order.class);
